272adc3337f7ed2392481f8e7d5915b5da8f5a2a
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-02-10 21:11:44 +0800

    style: implement a native splash screen (supports light and dark mode)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'firebase_options.dart';
 import 'router/app_router.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Initialize Firebase.
   await Firebase.initializeApp(
@@ -14,6 +17,11 @@ void main() async {
   );
 
   runApp(const ScholarsGuideApp());
+
+  // TODO: [P1] Investigate unsual crashing of the app after going home and
+  // then going back to the app (this happened after using the preserve
+  // and remove feature of the FlutterNativeSplash package).
+  FlutterNativeSplash.remove();
 }
 
 class ScholarsGuideApp extends StatelessWidget {
