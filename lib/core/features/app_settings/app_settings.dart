c05e662ac71d1ceb83a8865730b811f13af7bcb8
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-02-11 14:51:23 +0800

    dev: setup the folder structure for the core features: auth, app_settings, and system

---
+++ lib/core/features/app_settings/app_settings.dart
@@ -0,0 +1,2 @@
+// TODO: [P1] Users will be able to customize their app settings.
+// Example features include: theme, language, general localization, etc.
---
+++ lib/core/features/system/system.dart
@@ -0,0 +1,4 @@
+// TODO: [P2] Anything to do with the overall system of the app
+// (both frontend and backend). Mainly, this feature will be used by
+// the administrators. Sample features include: database management,
+// user management, analytics, etc.
